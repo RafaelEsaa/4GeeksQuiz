b74102de288d32ea86bef24a953c0e848e0126c3
Rafael Esaa <esaarafael@hotmail.com>
2019-04-22 13:44:11 -0400

    uploading Project to github

---
+++ README.md
@@ -0,0 +1,5 @@
+1. ``git clone``
+2. ``npm install``
+3. ``npm run start``
+
+App with React Js, download a json from the API and create another json in a Form like Google Form
---
+++ src/js/components/GetQuiz.js
@@ -0,0 +1,54 @@
+import React from 'react';
+
+import 'jquery';
+
+export default class GetQuiz extends React.Component {
+	constructor(){
+        super();
+        
+        this.state = {
+            value: 'https://assets.breatheco.de/apis/quiz/html'
+        }
+    }
+    
+    handleSubmit(event){
+        event.preventDefault();
+        this.props.onSelect(this.state.value);
+        this.setState({
+            value: ''
+        });
+    }
+
+    handleChange(event){
+        this.setState({
+            value: event.target.value
+        });
+    }
+
+	render () {
+		return (
+            <div className="alert alert-primary">
+                <h4 className="alert-heading">Load Quiz from URL</h4>
+                <form onSubmit={this.handleSubmit.bind(this)}>
+                  <div className="form-row">
+                    <div className="form-group col-md-8">
+                        <input 
+                            className="form-control" 
+                            type="text" 
+                            value={this.state.value} 
+                            onChange={this.handleChange.bind(this)} 
+                            placeholder="JSON URL HERE"
+                        />
+                    </div>
+                    <div className="form-group col">
+                        <button type="submit" className="btn btn-light form-control">Load quiz</button>
+                    </div>
+                    <div className="form-group col">
+                        <button type="button" className="btn btn-light form-control">Start a new one</button>
+                    </div>
+                  </div>
+                </form>
+            </div>
+		);
+	}
+}
